bba8d889354ed26420bebab39da84449957f2124
g1ann0z <giannozcydia@gmail.com>
2024-01-12 18:56:56 +0100

    pw-sql

--- Node/app/config/config.js
+++ Node/app/config/config.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize({
   database: "tnv_final_project",
   username: "root",
-  password: "R00t@!",
+  password: "Normix200",
   host: "localhost",
   port: 3306,
   dialect: "mysql",
